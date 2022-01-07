11860d6f0d1142c154f52614aea27916fff4b470
Commoble <josephbettendorff@yahoo.com>
2022-01-06 18:46:57 -0600

    Update to 1.18.1

--- src/examplemod/java/commoble/autopalette/examplemod/AutopaletteExampleMod.java
+++ src/examplemod/java/commoble/autopalette/examplemod/AutopaletteExampleMod.java
@@ -1,12 +1,11 @@
 package commoble.autopalette.examplemod;
 
-import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.IForgeRegistry;
@@ -22,9 +21,9 @@ public class AutopaletteExampleMod
 		IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
 		
 		DeferredRegister<Block> blocks = makeDeferredRegister(modBus, ForgeRegistries.BLOCKS);
-		RegistryObject<Block> darkCobblestone = blocks.register("dark_cobblestone", () ->
+		blocks.register("dark_cobblestone", () ->
 			new Block(Properties.copy(Blocks.COBBLESTONE)));
-		RegistryObject<Block> greenMagma = blocks.register("green_magma", () ->
+		blocks.register("green_magma", () ->
 			new Block(Properties.copy(Blocks.MAGMA_BLOCK)));
 	}
 	
